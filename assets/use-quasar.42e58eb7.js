62e797cac155b4763a7bfe768b0f19c9ddfdb485
AtanasRaykovPapucharov <atanas.papucharov@gmail.com>
2024-10-20 23:09:54 +0300

    update

---
+++ assets/use-quasar.42e58eb7.js
@@ -0,0 +1,1 @@
+import{c as u,i as e,a5 as s}from"./index.d2c78ec9.js";const n={dark:{type:Boolean,default:null}};function o(a,r){return u(()=>a.dark===null?r.dark.isActive:a.dark)}function c(){return e(s)}export{n as a,c as b,o as u};
